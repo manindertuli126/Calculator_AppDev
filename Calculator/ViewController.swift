ba0556a473569444339c0ca4359455d8012db79f
maninder <maninder@maninders-Mac.local>
2019-03-06 11:11:27 -0500

    Initial Commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by maninder on 2019-03-06.
+//  Copyright © 2019 manindersingh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
